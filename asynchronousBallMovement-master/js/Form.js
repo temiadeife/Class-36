df44dd9e29090281ab822fbae6704642da445d27
temiadeife <69051649+temiadeife@users.noreply.github.com>
2020-10-30 20:08:10 -0500

    Add files via upload

---
+++ asynchronousBallMovement-master/js/Form.js
@@ -0,0 +1,38 @@
+class Form {
+    constructor() {
+      this.input = createInput("name");
+      this.button = createButton('play');
+      this.greeting = createElement('h2');
+    }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+    display(){
+      var title = createElement('h2')
+      title.html("Car Racing Game");
+      title.position(250, 0);
+      
+      var input = createInput("Name");
+      var button = createButton('Play');
+      
+      this.input.position(250, 200);
+     this.button.position(350, 350);
+  
+      this.button.mousePressed(()=>{
+        this.input.hide();
+        this.button.hide();
+  
+        player.name = this.input.value();
+        
+        playerCount+=1;
+        player.index = playerCount;
+        player.update()
+        player.updateCount(playerCount);
+        this.greeting.html("Hello " + player.name )
+        this.greeting.position(130, 160)
+      });
+  
+    }
+  }
